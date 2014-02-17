2e520e64e7eb4b38689289f10acf6a0dc0aede0c
mczhao <mingchen.zhao@gmail.com>
2014-02-17 09:36:00 -0500

    add one

---
+++ Single_Number_II.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int singleNumber(int[] A) {
+        int one = 0; 
+        int two = 0; 
+        int three = 0;
+        for (int i=0; i<A.length; i++) {
+            two |= (one & A[i]);
+            one ^= A[i];
+            three = one & two;
+            one &= (~three);
+            two &= (~three);
+        }
+        return one;
+    }
+}
