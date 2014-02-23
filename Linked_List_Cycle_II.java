aab282d901e3f6c3a5d9a4f7f970b27062056d81
mczhao <mingchen.zhao@gmail.com>
2014-02-23 15:23:04 -0500

    add one

--- Linked_List_Cycle_II.java
+++ Linked_List_Cycle_II.java
@@ -10,17 +10,22 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head==null) return false;
-        HashSet<ListNode> set = new HashSet<ListNode>();
-        ListNode p = head;
-        ListNode q = head.next;
-        while ((p!=null) && (q!=null)) {
-            if (p==q) return true;
+    public ListNode detectCycle(ListNode head) {
+        if (head==null) return null;
+        ListNode p = head.next;
+        ListNode q = null;
+        if (p!=null) q = p.next;
+        while ((p!=q) && (p!=null) && (q!=null)) {
             p = p.next;
             q = q.next;
             if (q!=null) q=q.next;
         }
-        return false;
+        if ((p==null) || (q==null)) return null;
+        p = head;
+        while (p!=q) {
+            p = p.next;
+            q = q.next;
+        }
+        return p;
     }
 }
