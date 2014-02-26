b12cb894a3be0e6c4e4f6877c43bf0108df35585
mczhao <mingchen.zhao@gmail.com>
2014-02-26 14:07:25 -0500

    add one

--- Search_in_Rotated_Sorted_Array_II.java
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -1,20 +1,20 @@
 public class Solution {
-    public int search(int[] A, int target) {
+    public boolean search(int[] A, int target) {
         int n = A.length;
-        if (n==0) return -1;
+        if (n==0) return false;
         int l = 0;
-        int r = A.length - 1;
+        int r = n-1;
         while (l<=r) {
             int m = (l+r)/2;
-            if (A[m]==target) return m;
-            if (A[l]<=A[m]) {
+            if (A[m]==target) return true;
+            if (A[l]<A[m]) {
                 if ((A[l]<=target) && (target<A[m])) r = m - 1;
                 else l = m + 1;
-            } else {
+            } else if (A[l]>A[m]) {
                 if ((A[m]<target) && (target<=A[r])) l = m + 1;
                 else r = m - 1;
-            }
+            } else l++;
         }
-        return -1;
+        return false;
     }
 }
